5cbce19339b32720a5c65def9e1fd13a0e9c05b6
Gareth Rushgrove <gareth@morethanseven.net>
2016-07-21 14:54:49 +0100

    Added comments to be picked up by godoc

--- main.go
+++ main.go
@@ -1,3 +1,15 @@
+// dockerfilepp-puppet is a tool which adds new instructions to Dockerfile for
+// using Puppet as part of an image build process.
+//
+// It does that by post-processing a standard Dockerfile and replacing new
+// instructions with valid snippets of Dockerfile.
+//
+// Under the hood dockerfilepp-puppet uses the dockerfilepp library.
+//
+// Usage:
+//
+//    dockerfilepp-puppet < Dockerfile
+//
 package main
 
 import (
@@ -11,13 +23,13 @@ func main() {
 		content, _ := Asset(file)
 		replacements[file] = string(content)
 	}
-	dockerfilepp.Process(replacements, `dockerfilepp is a tool for adding new instructions to Dockerfile
+	dockerfilepp.Process(replacements, `dockerfilepp-puppet is a tool for adding new instructions to Dockerfile
 
 Usage:
 
     dockerfilepp-puppet < Dockerfile
     Dockerfile | dockerfilepp-puppet
 
-dockerfilepp takes a Dockerfile on stdin and outputs to stdout a modified version
+dockerfilepp-puppet takes a Dockerfile on stdin and outputs to stdout a modified version
 of the same Dockerfile which has been through the registered pre-processors.`)
 }
